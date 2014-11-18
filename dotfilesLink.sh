010ddb5b4da87b252e0406598af9c7715ead7aec
chansuke <moonset20@gmail.com>
2014-11-18 20:48:36 +0900

    update script

--- dotfilesLink.sh
+++ dotfilesLink.sh
@@ -3,3 +3,5 @@ ln -s ~/dotfiles/.vim ~/.vim
 ln -s ~/dotfiles/.vimrc ~/.vimrc
 ln -s ~/dotfiles/.zprofile ~/.zprofile
 ln -s ~/dotfiles/.gitconfig ~/.gitconfig
+ln -s ~/dotfiles/.tmux.conf ~/.tmux.conf
+ln -s ~/dotfiles/.zshrc ~/.zshrc
